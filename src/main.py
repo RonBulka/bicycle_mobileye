c5a3710519caeac6a89e1be3743c044e374e909e
RonBulka <lebedinskyron@gmail.com>
2025-01-23 18:14:38 +0200

    before training model

--- src/main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import fiftyone as fo
-
-if __name__ == '__main__':
-    print('Hello, World!')
---
+++ src/train_yolo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import torch
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    print(f"Is CUDA available? {torch.cuda.is_available()}")
+    if torch.cuda.is_available():
+        print(f"Using device: {torch.cuda.get_device_name(0)}")
+    else:
+        print("No GPU detected. Training will run on the CPU.")
+
+    config_path = './config.yaml'
+
+    # Load a pre trained model
+    model = YOLO("yolov10n.pt")
+
+    # Set the device to the ROCm-supported GPU
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+
+    # train the model
+    model.train(data=config_path, epochs=200, batch=32)
