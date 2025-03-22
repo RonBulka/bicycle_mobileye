47c2aca0f61aed5da9c10451cd181a3b2dbc5f67
RonBulka <lebedinskyron@gmail.com>
2025-03-22 15:12:51 +0200

    Add argparser to train_yolo.py

--- src/train_yolo.py
+++ src/train_yolo.py
@@ -1,29 +1,76 @@
 #!/usr/bin/env python
-import os
 import torch
+import argparse
 from ultralytics import YOLO
 
-EPOCHS = 200
-BATCH_SIZE = 32
-IMAGE_SIZE = 640
+EPOCHS      = 200
+BATCH_SIZE  = 32
+IMAGE_SIZE  = 640
+CONFIG      = './dataset/dataset.yaml'
+MODEL       = 'yolov8n.pt'
+OUTPUT_DIR  = './runs'
+
+# add args to the script
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train YOLO model')
+    parser.add_argument(
+        '--model', '-m',
+        type=str,
+        default=MODEL,
+        help=f'Path to the pre-trained model, default is {MODEL}'
+    )
+    parser.add_argument(
+        '--epochs', '-e',
+        type=int,
+        default=EPOCHS,
+        help=f'Number of epochs to train the model, default is {EPOCHS}'
+    )
+    parser.add_argument(
+        '--batch', '-b',
+        type=int,
+        default=BATCH_SIZE,
+        help=f'Batch size for training, default is {BATCH_SIZE}'
+    )
+    parser.add_argument(
+        '--imgsz', '-i',
+        type=int,
+        default=IMAGE_SIZE,
+        help=f'Image size for training, default is {IMAGE_SIZE}'
+    )
+    parser.add_argument(
+        '--config', '-c',
+        type=str,
+        default=CONFIG,
+        help=f'Path to the dataset configuration file, default is {CONFIG}'
+    )
+    parser.add_argument(
+        '--output', '-o',
+        type=str,
+        default=OUTPUT_DIR,
+        help=f'Output directory for the training results, default is {OUTPUT_DIR}'
+    )
+    return parser.parse_args()
 
 if __name__ == '__main__':
+    args = parse_args()
     print(f"Is CUDA available? {torch.cuda.is_available()}")
     if torch.cuda.is_available():
         print(f"Using device: {torch.cuda.get_device_name(0)}")
     else:
         print("No GPU detected. Training will run on the CPU.")
 
-    config_path = './dataset/dataset.yaml'
-
     # Load a pre trained model
-    model = YOLO("yolov8n.pt")
+    model = YOLO(args.model)
 
     # Set the device to the ROCm-supported GPU
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
 
     # train the model
-    model.train(data=config_path, epochs=EPOCHS, batch=BATCH_SIZE, imgsz=IMAGE_SIZE, device=device, project="./runs")
-
-# TODO: add command line arguments to the script
+    model.train(data=args.config,
+                epochs=args.epochs,
+                batch=args.batch,
+                imgsz=args.imgsz,
+                device=device,
+                project=args.output
+    )
